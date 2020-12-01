d823e05ba299169c4f712bd027464a3cdc0ebc5a
skyon16 <mille4kg@dukes.jmu.edu>
2020-12-01 16:25:00 -0500

    added front end feather api

--- src/Unbrewd.js
+++ src/Unbrewd.js
@@ -4,10 +4,19 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import { useState } from 'react';
+import api from './services/api'
 
-var insertVarName = 21;
 
 function Unbrewd() {
+    const [count, setCount] = useState(21)
+    const [news, setNews] = useState([])
+    api.service('news').find({}).then(
+        result => {
+            console.log(result.data)
+            setNews(result.data)
+        }
+    )
     return(
         <div className="Unbrewd">
             <header className="Unbrewd-header">
@@ -19,11 +28,11 @@ function Unbrewd() {
                             Test content
                         </Typography>
                         <Typography variant="h5" component="h2">
-                            {insertVarName}
+                            {news}
                         </Typography>
                     </CardContent>
                     <CardActions>
-                        <Button size="small" variant="contained" color="secondary" onClick={() => {insertVarName++}}>Click Me</Button>
+                        <Button size="small" variant="contained" color="secondary" onClick={() => {setCount(count + 1)}}>Click Me</Button>
                     </CardActions>
                 </Card>
             </header>
